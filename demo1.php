9fd9569601fb3a81c0d2d48172d36161c4af08af
Jagat <jagat2205114@gmail.com>
2019-08-06 21:42:10 +0530

    added star program

--- demo1.php
+++ demo1.php
@@ -1,3 +1,12 @@
 <?php
 
-phpinfo();
+function star($num) {
+    for($i=$num; $i>=1; $i--) {
+        for($j=1; $j<=$i; $j++) {
+            echo '* ';
+        }
+        echo PHP_EOL;
+    }
+}
+
+star(5);
